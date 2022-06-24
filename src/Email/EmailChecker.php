c4f57f3d850063ccde3d8892d6ff9610a50d6607
gdarquie <gaetan.darquie@gmail.com>
2022-06-25 00:02:46 +0200

    First working Service with Email checker

--- src/Email/EmailChecker.php
+++ src/Email/EmailChecker.php
@@ -6,16 +6,18 @@ class EmailChecker
 {
     const EMAIL_CHECK_URL = 'https://open.api.bouyguestelecom.fr/v1/customer-management/email-addresses/check';
 
-    public function __invoke(string $emailAddress, string $url=self::EMAIL_CHECK_URL): array
+    public function __invoke(string $emailAddress, $authenticator, $client, string $url = self::EMAIL_CHECK_URL): array
     {
         // add content to body request
         $options['json'] = ['emailAddress' => $emailAddress];
 
         // authentication with app credentials flow: get token
-        $options['auth_bearer'] = $this->authenticator->authenticate();
+        $options['auth_bearer'] = $authenticator->authenticate();
 
         // get response
-        return $this->request($this->client, 'POST', $url, $options);
+        $response = $client('POST', $url, $options);
+
+        return $response->toArray();
     }
 }
 
